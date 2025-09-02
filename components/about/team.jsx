69e2bb5c5caafdad70b2a43eb749b24390a18785
IcaroVazz <icarovaz952@gmail.com>
2025-09-02 14:01:26 -0300

    Subindo

--- components/about/team.jsx
+++ components/about/team.jsx
@@ -6,7 +6,7 @@ import HeadLine from "../headLine";
 const Team = () => {
   return (
     <div>
-      {/* <!-- Team -aaaaaasss-> */}
+      {/* <!-- Team -aaaaaaaasss-> */}
       <section className="py-24">
         <div className="container">
           <HeadLine
